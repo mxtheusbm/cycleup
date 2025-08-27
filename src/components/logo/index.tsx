f4511dae71b46bc88f2dedc1dba8182bda17fada
Matheus Barbosa <mxtheusbm@gmail.com>
2025-08-26 22:12:43 -0300

    fix: change tag [a] to link

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,14 +1,15 @@
 import { TimerIcon } from "@phosphor-icons/react";
 
 import styles from "./styles.module.css";
+import { RouterLink } from "../router-link";
 
 export const Logo = () => {
   return (
     <div className={styles.logo}>
-      <a className={styles.logoLink} href="/">
+      <RouterLink className={styles.logoLink} href="/">
         <TimerIcon />
         <span>Cycleup</span>
-      </a>
+      </RouterLink>
     </div>
   );
 };
